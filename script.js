25a21bafd4a17c1f6b1540882ad303ae274b4411
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2023-06-30 01:20:18 -0400

    Update info displayed for today

--- script.js
+++ script.js
@@ -37,17 +37,54 @@ function populate() {
   while (today[0].firstChild) {
     today[0].removeChild(today[0].lastChild);
   }
-  for (let i = 0; i < Object.keys(weatherData.current).length; i++) {
-    let div = document.createElement("div");
-    div.classList.add("keys");
-    div.textContent = Object.keys(weatherData.current)[i];
-    let div2 = document.createElement("div");
-    div2.classList.add("values");
-    div2.textContent = Object.values(weatherData.current)[i];
-    today[0].appendChild(div);
-    today[0].appendChild(div2);
-  }
-  // Today above
+  // for (let i = 0; i < Object.keys(weatherData.current).length; i++) {
+  //   let div = document.createElement("div");
+  //   div.classList.add("keys");
+  //   div.textContent = Object.keys(weatherData.current)[i];
+  //   let div2 = document.createElement("div");
+  //   div2.classList.add("values");
+  //   div2.textContent = Object.values(weatherData.current)[i];
+  //   today[0].appendChild(div);
+  //   today[0].appendChild(div2);
+  // }
+
+  let date = document.createElement("div");
+  date.classList.add("date");
+  date.textContent = weatherData.current.date;
+  today[0].appendChild(date);
+
+  let maxTemp = document.createElement("div");
+  maxTemp.classList.add("maxTemp");
+  maxTemp.textContent = "High: " + weatherData.current.day.maxtemp_f + "\xB0";
+  today[0].appendChild(maxTemp);
+
+  let minTemp = document.createElement("div");
+  minTemp.classList.add("minTemp");
+  minTemp.textContent = "Low: " + weatherData.current.day.mintemp_f + "\xB0";
+  today[0].appendChild(minTemp);
+
+  let chanceRain = document.createElement("div");
+  chanceRain.classList.add("chanceRain");
+  chanceRain.textContent =
+    "Rain: " + weatherData.current.day.daily_chance_of_rain + "%";
+  today[0].appendChild(chanceRain);
+
+  let conditionIcon = document.createElement("img");
+  conditionIcon.classList.add("conditionIcon");
+  conditionIcon.src = weatherData.current.day.condition.icon;
+  today[0].appendChild(conditionIcon);
+
+  let sunrise = document.createElement("div");
+  sunrise.classList.add("sunrise");
+  sunrise.textContent = "Sunrise: " + weatherData.current.astro.sunrise;
+  today[0].appendChild(sunrise);
+
+  let sunset = document.createElement("div");
+  sunset.classList.add("sunset");
+  sunset.textContent = "Sunset: " + weatherData.current.astro.sunset;
+  today[0].appendChild(sunset);
+
+  // Today above **********************************
   // Forecast days below
   const day = document.getElementsByClassName("day");
   for (let i = 0; i < day.length; i++) {
